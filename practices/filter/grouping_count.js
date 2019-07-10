19e49a7cc9e07152c3a811f5a12b2bcbc0a1bb9c
LIUST3 <stefan.liu@oocl.com>
2019-07-10 15:00:36 +0800

    finish map homework

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,8 @@
 function grouping_count(collection) {
 
   //implement here
+  return collection.reduce((key,value) => (key[value]++ || (key[value] = 1),key),{});
 }
+
 
 module.exports = grouping_count;
