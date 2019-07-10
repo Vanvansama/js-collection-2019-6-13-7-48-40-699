1ac377fe11acaddd2473e2246cd9236117c3cf1f
LIUST3 <stefan.liu@oocl.com>
2019-07-10 18:10:34 +0800

    learn reduce

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,7 +3,15 @@
 function grouping_count(collection) {
 
   //implement here
-  return collection.reduce((key,value) => (key[value]++ || (key[value] = 1),key),{});
+  // return collection.reduce((key,value) => (key[value]++ || (key[value] = 1),key),{});
+  return collection.reduce((object,collectionItem) => {
+    if (object[collectionItem]) {
+      object[collectionItem]++;
+    } else {
+      object[collectionItem] = 1;
+    }
+    return object
+  },{});
 }
 
 
